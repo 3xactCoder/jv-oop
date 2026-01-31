da6157637f5d294e8b9f3984469256de3fff5b05
3xacT <vovaoliynyk99@gmail.com>
2026-01-31 23:15:41 +0200

    done all tasks from README.md

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,16 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Machine first = new Excavator();
+        Machine second = new Truck();
+        Machine third = new Bulldozer();
+        Machine[] machines = {first, second, third};
+        for (Machine comp : machines) {
+            comp.doWork();
+            comp.stopWork();
 
+        }
+    }
 }
+
